c6c14759a3082c51833171e005e8018009de9489
Aman <mandalamankumar.786@gmail.com>
2023-09-21 10:49:13 +0530

    question page updated

--- loader.js
+++ loader.js
@@ -1,6 +1,7 @@
 // JavaScript to hide the loader when the content is loaded
 window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader-wrapper");
-    console.log('loading')
-    loader.style.display = "none";
-  });
+  const loader = document.querySelector(".loader-wrapper");
+  const loadingText = document.querySelector(".loading-text");
+  loader.style.display = "none";
+  loadingText.style.display = "none";
+});
